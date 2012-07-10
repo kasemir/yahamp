4f60670e26c5e7cb51b6ff125dde65fcd5b96622
Kay <Kay.Kasemir@gmail.com>
2012-07-09 20:57:20 -0400

    NPE in grid display on map

--- yahamp.ui/src/yahamp/ui/map/MapDisplayPart.java
+++ yahamp.ui/src/yahamp/ui/map/MapDisplayPart.java
@@ -6,6 +6,10 @@
  *******************************************************************************/
 package yahamp.ui.map;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -64,14 +68,24 @@ public class MapDisplayPart
 	 */
 	public void mark(final Object... locations_and_texts)
 	{
+	    mark(Arrays.asList(locations_and_texts));
+	}
+
+	/** Set markers to display on map
+     *  @param locations_and_texts List of locations and associated texts.
+     *         Must be an array of Position 1, label 1, Position 2, label 2, ...
+     */
+	public void mark(final List<Object> locations_and_texts)
+	{
 	    earth.clearMarkers();
-	    if ((locations_and_texts.length % 2) != 0)
+	    if ((locations_and_texts.size() % 2) != 0)
 	        throw new IllegalArgumentException("Need matched number of locations and texts");
-	    for (int i=0; i<locations_and_texts.length; i+=2)
+	    for (int i=0; i<locations_and_texts.size(); i+=2)
 	    {
-	        if (! (locations_and_texts[i] instanceof Position))
-	            throw new IllegalArgumentException("Expected location, got " + locations_and_texts[i]);
-	        earth.addMarker((Position) locations_and_texts[i], locations_and_texts[i+1].toString());
+	        if (! (locations_and_texts.get(i) instanceof Position))
+	            throw new IllegalArgumentException("Expected location, got " + locations_and_texts.get(i));
+	        earth.addMarker((Position) locations_and_texts.get(i),
+	                        locations_and_texts.get(i+1).toString());
 	    }
 	}
 
@@ -84,13 +98,19 @@ public class MapDisplayPart
    @Optional
    public void markCall(@UIEventTopic(Activator.TOPIC) final CallInfo callinfo)
    {
-       if (callinfo == null)
-           mark();
-       else if (my_location != null  &&  my_call != null)
-           mark(my_location, my_call,
-                Maidenhead.fromGrid(callinfo.getGrid()), callinfo.getCall());
-       else
-           mark(my_location, my_call,
-                   Maidenhead.fromGrid(callinfo.getGrid()), callinfo.getCall());
+       final List<Object> markers = new ArrayList<>();
+       if (my_location != null  &&  my_call != null)
+       {
+           markers.add(my_location);
+           markers.add(my_call);
+       }
+       final String grid = callinfo.getGrid();
+       if (! grid.isEmpty())
+       {
+           final Maidenhead location = Maidenhead.fromGrid(grid);
+           markers.add(location);
+           markers.add(callinfo.getCall());
+       }
+       mark(markers);
    }
 }
